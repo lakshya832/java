3870bd9dc97a4a6b3d0f512ca32a66d3584aa2b0
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-11-30 22:22:38 +0530

    .

---
+++ src/com/demo/datatypes/test.java
@@ -0,0 +1,11 @@
+package com.demo.datatypes;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 1;
+        byte b = 1;
+        long c = 1;
+
+        System.out.println(b+c);
+    }
+}
