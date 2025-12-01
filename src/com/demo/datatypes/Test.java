09c6eed0cdad4f98b55b0c45b2878a51a716392d
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-01 16:50:03 +0530

    replace

--- src/com/demo/datatypes/Test.java
+++ src/com/demo/datatypes/Test.java
@@ -1,8 +1,18 @@
 package com.demo.datatypes;
 
+import java.sql.SQLOutput;
+
 public class Test {
      static void main(String[] args) {
         String a = "Hello!";
+        //sout = System.out.println();
+        System.out.println(a.length());
+        System.out.println(a.charAt(5));
+        System.out.println(a.substring(0,3));
+        System.out.println(a.contains("Hell"));
+        System.out.println(a.startsWith("H"));
         System.out.println(a.endsWith("!"));
+        String b = a.replace("!", " World");
+        System.out.println(b);
     }
 }
